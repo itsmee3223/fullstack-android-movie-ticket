d6f4bcabd15f3468af7031e82d97c89d2b90d8a1
Ramanda <tamvam.pemberani@gmail.com>
2022-04-03 17:02:17 +0700

    Splashscreen, Onboarding, Signin

---
+++ app/src/main/java/com/ramanda/ticketmovie/onboarding/OnBoardingThreeActivity.kt
@@ -0,0 +1,12 @@
+package com.ramanda.ticketmovie.onboarding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ramanda.ticketmovie.R
+
+class OnBoardingThreeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding_three)
+    }
+}
