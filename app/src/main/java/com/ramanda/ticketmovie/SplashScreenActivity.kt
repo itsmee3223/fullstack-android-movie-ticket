18a9255bd03cc53f27c4cadc8146002069ef6ca5
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-10 13:17:41 +0700

    Splash Screen Activity

--- app/src/main/java/com/ramanda/ticketmovie/SplashScreenActivity.kt
+++ app/src/main/java/com/ramanda/ticketmovie/SplashScreenActivity.kt
@@ -1,11 +1,21 @@
 package com.ramanda.ticketmovie
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import com.ramanda.ticketmovie.onboarding.OnBoardingOneActivity
 
 class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this@SplashScreenActivity, OnBoardingOneActivity::class.java)
+                startActivity(intent)
+                finish()
+        }, 2000)
     }
 }
