a8bfca497080fbd055eb4236ccc7b08e97a41b46
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-13 13:23:22 +0700

    Home dashboard fragment

---
+++ app/src/main/java/com/ramanda/ticketmovie/home/HomeScreenActivity.kt
@@ -0,0 +1,57 @@
+package com.ramanda.ticketmovie.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.fragment.app.Fragment
+import com.ramanda.ticketmovie.DashboardFragment
+import com.ramanda.ticketmovie.R
+import com.ramanda.ticketmovie.SettingFragment
+import com.ramanda.ticketmovie.TiketFragment
+import com.ramanda.ticketmovie.databinding.ActivityHomeScreenBinding
+
+class HomeScreenActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHomeScreenBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val fragmentTiket = TiketFragment()
+        val fragmentSetting = SettingFragment()
+        val fragmentHome = DashboardFragment()
+
+        setFragment(fragmentHome)
+
+        binding.ivMenu1.setOnClickListener {
+            setFragment(fragmentTiket)
+            changeIcon(binding.ivMenu1, R.drawable.ic_tiket_active)
+            changeIcon(binding.ivMenu2, R.drawable.ic_home)
+            changeIcon(binding.ivMenu3, R.drawable.ic_profile)
+        }
+        binding.ivMenu2.setOnClickListener {
+            setFragment(fragmentHome)
+            changeIcon(binding.ivMenu1, R.drawable.ic_tiket)
+            changeIcon(binding.ivMenu2, R.drawable.ic_home_active)
+            changeIcon(binding.ivMenu3, R.drawable.ic_profile)
+        }
+        binding.ivMenu3.setOnClickListener {
+            setFragment(fragmentSetting)
+            changeIcon(binding.ivMenu1, R.drawable.ic_tiket)
+            changeIcon(binding.ivMenu2, R.drawable.ic_home)
+            changeIcon(binding.ivMenu3, R.drawable.ic_profile_active)
+        }
+    }
+
+    private fun changeIcon(imageView: ImageView, int: Int) {
+        imageView.setImageResource(int)
+    }
+
+    private fun setFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.layout_frame, fragment)
+        fragmentTransaction.commit()
+    }
+}
