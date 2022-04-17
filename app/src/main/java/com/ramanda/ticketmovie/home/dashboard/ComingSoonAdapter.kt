d6d69608429b979baa9ac34e66a644d06177ba7d
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-17 13:15:06 +0700

    Coming soon adapter

---
+++ app/src/main/java/com/ramanda/ticketmovie/home/dashboard/ComingSoonAdapter.kt
@@ -0,0 +1,56 @@
+package com.ramanda.ticketmovie.home.dashboard
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ramanda.ticketmovie.R
+import com.ramanda.ticketmovie.home.model.Film
+
+class ComingSoonAdapter(private var data: List<Film>,
+                        private var listener: (Film) -> Unit) : RecyclerView.Adapter<ComingSoonAdapter.LeagueViewHolder>() {
+
+    private lateinit var contextAdapter : Context
+    class LeagueViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val tvTitle: TextView = view.findViewById(R.id.tv_title)
+        private val tvGenre: TextView = view.findViewById(R.id.tv_genre)
+        private val tvRate: TextView = view.findViewById(R.id.tv_rate)
+
+        private  val tvImage: ImageView = view.findViewById(R.id.iv_poster_image)
+
+        fun bindItem(data: Film, listener: (Film) -> Unit, context: Context) {
+            tvTitle.text = data.judul
+            tvGenre.text = data.genre
+            tvRate.text = data.rating
+
+            Glide.with(context)
+                .load(data.poster)
+                .into(tvImage)
+
+            itemView.setOnClickListener {
+                listener(data)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): LeagueViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        contextAdapter = parent.context
+        val inflateView: View = layoutInflater.inflate(R.layout.coming_soon_item, parent, false)
+        return  LeagueViewHolder(inflateView)
+
+    }
+
+    override fun onBindViewHolder(holder: LeagueViewHolder, position: Int) {
+        holder.bindItem(data[position], listener, contextAdapter)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+}
