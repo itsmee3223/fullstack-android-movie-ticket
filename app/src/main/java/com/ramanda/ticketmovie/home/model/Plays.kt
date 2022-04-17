7c2fd2211fe019488b062f66107cc91d38b7efa7
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-17 11:55:55 +0700

    Model User

---
+++ app/src/main/java/com/ramanda/ticketmovie/home/model/Plays.kt
@@ -0,0 +1,10 @@
+package com.ramanda.ticketmovie.home.model
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Plays (
+    var nama: String ?="",
+    var url: String ?=""
+): Parcelable
