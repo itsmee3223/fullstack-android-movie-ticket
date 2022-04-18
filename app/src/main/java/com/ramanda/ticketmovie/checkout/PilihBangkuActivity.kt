b463fa42d1a695e2f4247c5106eb145a34bdb7af
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-18 11:25:16 +0700

    Pilih bangku

--- app/src/main/java/com/ramanda/ticketmovie/checkout/PilihBangkuActivity.kt
+++ app/src/main/java/com/ramanda/ticketmovie/checkout/PilihBangkuActivity.kt
@@ -1,11 +1,93 @@
-package com.ramanda.ticketmovie
+package com.ramanda.ticketmovie.checkout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.ramanda.ticketmovie.R
+import com.ramanda.ticketmovie.databinding.ActivityPilihBangkuBinding
+import com.ramanda.ticketmovie.model.Checkout
+import com.ramanda.ticketmovie.model.Film
 
 class PilihBangkuActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPilihBangkuBinding
+
+    private var statusA3: Boolean = false
+    private var statusA4: Boolean = false
+    private var total: Int = 0
+
+    private var dataList = ArrayList<Checkout>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pilih_bangku)
+        binding = ActivityPilihBangkuBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val data = intent.getParcelableExtra<Film>("data")
+
+        binding.tvTitle.text = data!!.judul
+
+        binding.ivKursiA3.setOnClickListener {
+            if (statusA3) {
+                binding.ivKursiA3.setImageResource(R.drawable.ic_rectangle_empty)
+                statusA3 = false
+                total -=1
+                belitiket(total)
+
+                // delete data
+                dataList.remove(Checkout("A3", "70000"))
+
+            } else {
+                binding.ivKursiA3.setImageResource(R.drawable.ic_rectangle_selected)
+                statusA3 = true
+                total +=1
+                belitiket(total)
+
+                val data = Checkout("A3", "70000")
+                dataList.add(data)
+            }
+        }
+
+        binding.ivKursiA4.setOnClickListener {
+            if (statusA4) {
+                binding.ivKursiA4.setImageResource(R.drawable.ic_rectangle_empty)
+                statusA4 = false
+                total -=1
+                belitiket(total)
+
+                // delete data
+                dataList.remove(Checkout("A4", "70000"))
+
+            } else {
+                binding.ivKursiA4.setImageResource(R.drawable.ic_rectangle_selected)
+                statusA4 = true
+                total +=1
+                belitiket(total)
+
+                val data = Checkout("A4", "70000")
+                dataList.add(data)
+            }
+        }
+
+        binding.btnBeli.setOnClickListener {
+
+            val intent = Intent(
+                this,
+                CheckoutActivity::class.java
+            ).putExtra("data", dataList).putExtra("datas", data)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun belitiket(total:Int) {
+        if (total == 0) {
+            binding.btnBeli.text = "Beli Tiket"
+            binding.btnBeli.visibility = View.INVISIBLE
+        } else {
+            binding.btnBeli.text = "Beli Tiket ($total)"
+            binding.btnBeli.visibility = View.VISIBLE
+        }
+
     }
 }
