eb6ecb04a2c4bb6b1c79766062999af8db6abce3
ramanda ajisaka <tamvam.pemberani@gmail.com>
2022-04-19 22:02:07 +0700

    Top up success

---
+++ app/src/main/java/com/ramanda/ticketmovie/home/wallet/MyWalletSuccessActivity.kt
@@ -0,0 +1,27 @@
+package com.ramanda.ticketmovie.home.wallet
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.ramanda.ticketmovie.databinding.ActivityMyWalletSuccessBinding
+import com.ramanda.ticketmovie.home.HomeScreenActivity
+import com.ramanda.ticketmovie.home.TiketScreenActivity
+
+class MyWalletSuccessActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMyWalletSuccessBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMyWalletSuccessBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.btnHome.setOnClickListener {
+            startActivity(Intent(this@MyWalletSuccessActivity, HomeScreenActivity::class.java))
+        }
+
+        binding.btnWallet.setOnClickListener {
+            startActivity(Intent(this@MyWalletSuccessActivity, TiketScreenActivity::class.java))
+        }
+    }
+}
